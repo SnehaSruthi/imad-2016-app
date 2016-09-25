8917f1619d1734598d57546cdb0d357a94ed157e
SnehaSruthi <snehasruthi27@gmail.com>
2016-09-25 23:47:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,17 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
+
+    // Make the request
+        request.open('GET', 'http://snehasruthi.imad.hasura-app.io/submit-name=' + name, true);
+        request.send(null);
+};
+
+//Submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
     
+
     // Create a request object
     var request = new XMLHttpRequest();
     
@@ -11,9 +21,15 @@ button.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             // Take some action
             if (request.status === 200) {
-                var counter = request.respnseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                 //Capture a list of names and render it as a list
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for(var i=0; i< names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
             }
         }
             // Not done yet
@@ -21,22 +37,8 @@ button.onclick = function () {
     };
     
         // Make the request
-        request.open('GET', 'http://snehasruthi.imad.hasura-app.io/counter', true);
+        var nameInput = document.getElementById('name');
+        var name = nameInput.value;
+        request.open('GET', 'http://snehasruthi.imad.hasura-app.io/submit-name=' + name, true);
         request.send(null);
-};
-
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-    //Make a request to the server and send the name
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 };
