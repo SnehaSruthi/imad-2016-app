d06d2dd857a3c7994f9e5d6820db8352a18b54a0
SnehaSruthi <snehasruthi27@gmail.com>
2016-09-25 23:09:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ button.onclick = function () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             // Take some action
-            if(request.status === 200){
+            if (request.status === 200) {
                 var counter = request.respnseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
@@ -23,4 +23,20 @@ button.onclick = function () {
         // Make the request
         request.open('GET', 'http://snehasruthi.imad.hasura-app.io/counter', true);
         request.send(null);
+};
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //Make a request to the server and send the name
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
